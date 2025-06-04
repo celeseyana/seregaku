28f29dcb70cfdb30349983e88a567ab40c7a4998
Ong Zi Yang <celeskyanpu@gmail.com>
2025-06-04 18:58:58 +0800

    added login form

--- seregaku/src/pages/Login.jsx
+++ seregaku/src/pages/Login.jsx
@@ -1,9 +1,44 @@
 export default function Login() {
     return (
-        <>
-            <div>
-                login page
+        <section className="hero is-fullheight is-flex is-justify-content-center is-align-items-center">
+            <div className="box" style={{ width: '400px' }}>
+                <h1 className="title is-4 has-text-centered mb-5">Login</h1>
+
+                <div className="field">
+                    <p className="control has-icons-left has-icons-right">
+                        <input className="input" type="email" placeholder="Email" />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-envelope"></i>
+                        </span>
+                    </p>
+                </div>
+
+                <div className="field">
+                    <p className="control has-icons-left">
+                        <input className="input" type="password" placeholder="Password" />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-lock"></i>
+                        </span>
+                    </p>
+                </div>
+
+                <div className="field">
+                    <p className="control">
+                        <button className="button is-success is-fullwidth">
+                            Login
+                        </button>
+                    </p>
+                </div>
+
+                <div className="has-text-centered mt-4">
+                    <p>
+                        Don't have an account?{' '}
+                        <a href="" className="has-text-link">
+                            Sign up!
+                        </a>
+                    </p>
+                </div>
             </div>
-        </>
-    )
-};
+        </section>
+    );
+}
